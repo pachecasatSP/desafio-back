8152b572a6367290b533619becaeacaf9dcaf185
Adolfo Sales Pacheco <adolfo.pacheco@gmail.com>
2024-09-09 12:08:20 -0300

    deleting unnecessary files

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-# entrypoint.sh
-#!/bin/bash
-set -e
-
-# Executa as migrations
-dotnet ef database update --project desafio-back.infrastructure --startup-project desafio-back.api
-
-# Inicia a aplicação
-exec dotnet desafio-back.api.dll
